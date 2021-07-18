f768cc8355bcc9d1ebdcb583c2ed0433f93b4e3e
yuekaim <yuekaim@gmail.com>
2021-07-19 01:48:06 +0200

    accounts

---
+++ config/accounts.php
@@ -0,0 +1,3 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!');
+return unserialize('a:3:{s:4:"cleo";s:60:"$2y$10$qbIo1jP3Sg0869n4fOCRe.XCah.ZpE5qPQWZ2UjYuUAbrCEBQG.U6";s:10:"user_e4ecf";s:60:"$2y$10$YFkJHtmX3GQ5pnjrdNqDI.rZSvZ21CJ/pl3qlU8hUcatFn88E1nHC";s:2:"yi";s:60:"$2y$10$jOrLKHkX2dFb6fVQ1hB3x.XGKg9a9VBwrwQKWTwV2hRPHldWBAZOy";}');
+?>
